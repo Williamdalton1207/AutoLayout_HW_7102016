556c0b22be418db410dbf7a471740605c700dcfe
William Dalton <billdalton007@gmail.com>
2016-07-14 13:01:31 -0400

    trying to publish

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout_HW_7102016
+//
+//  Created by William Dalton on 7/11/16.
+//  Copyright © 2016 William Dalton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
